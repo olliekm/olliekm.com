b75483c0351e14229d6ddb768f4c027d533f2e92
Oliver <revilokm@gmail.com>
2023-07-17 14:25:54 -0400

    theme color

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout'
 function MyApp({ Component, pageProps }) {
   return(
     <Layout>
+      <meta name="theme-color" content="#6366f1"></meta>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <Component {...pageProps} />
     </Layout>
